f4b4af81b93e96249916ea2be8bfc8557734cc9e
ukyo1114 <xjr1300sunny@gmail.com>
2025-01-07 21:25:08 +0900

    広告表示用スクリプトを追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import './globals.css';
 import Link from 'next/link';
 
+export const metadata = {
+  title: 'My AdSense Enabled Site',
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -8,6 +12,23 @@ export default function RootLayout({
 }>) {
   return (
     <html>
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6870293550717146"
+          crossOrigin="anonymous"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (adsbygoogle = window.adsbygoogle || []).push({
+                google_ad_client: "ca-pub-XXXXXXXXXXXXXX",
+                enable_page_level_ads: true
+              });
+            `,
+          }}
+        />
+      </head>
       <body>
         <header>
           <Link href="/">筋トレ情報ポータル -Lift-</Link>
